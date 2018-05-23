405275309dae36e1d61aadcff9cd6612b451c1ab
OSCAR <OSCAR@10.30.4.10>
2018-05-22 20:19:48 -0500

    Primer push a github

---
+++ src/main/java/org/unitec/elementos1803/Elementos1803Application.java
@@ -0,0 +1,12 @@
+package org.unitec.elementos1803;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elementos1803Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elementos1803Application.class, args);
+	}
+}
